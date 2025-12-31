75a93a5846b0215718439f3c1db9de6c0dc36857
Tom Softreck <tom@sapletta.com>
2025-12-31 17:51:03 +0100

    draft

--- jest.config.js
+++ jest.config.js
@@ -1,62 +1,48 @@
 /** @type {import('jest').Config} */
+const tsJestTransform = ['ts-jest', {
+  tsconfig: 'tsconfig.json'
+}];
+
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/tests', '<rootDir>/dsl', '<rootDir>/core', '<rootDir>/api'],
-  testMatch: [
-    '**/__tests__/**/*.ts',
-    '**/*.test.ts',
-    '**/*.spec.ts'
-  ],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json'
-    }]
-  },
   moduleNameMapper: {
     '^@dsl/(.*)$': '<rootDir>/dsl/$1',
     '^@core/(.*)$': '<rootDir>/core/$1',
     '^@modules/(.*)$': '<rootDir>/modules/$1',
     '^@api/(.*)$': '<rootDir>/api/$1'
   },
-  collectCoverageFrom: [
-    'dsl/**/*.ts',
-    'core/**/*.ts',
-    'api/**/*.ts',
-    'modules/**/*.ts',
-    '!**/*.d.ts',
-    '!**/node_modules/**'
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60
-    }
-  },
-  coverageReporters: ['text', 'lcov', 'html'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  testTimeout: 30000,
   verbose: true,
   
   // E2E tests configuration
   projects: [
     {
       displayName: 'unit',
+      preset: 'ts-jest',
       testMatch: ['<rootDir>/tests/unit/**/*.test.ts'],
-      testEnvironment: 'node'
+      testEnvironment: 'node',
+      transform: {
+        '^.+\\.tsx?$': tsJestTransform
+      },
+      setupFilesAfterEnv: ['<rootDir>/tests/setup.ts']
     },
     {
       displayName: 'integration',
+      preset: 'ts-jest',
       testMatch: ['<rootDir>/tests/integration/**/*.test.ts'],
       testEnvironment: 'node',
+      transform: {
+        '^.+\\.tsx?$': tsJestTransform
+      },
       setupFilesAfterEnv: ['<rootDir>/tests/integration/setup.ts']
     },
     {
       displayName: 'e2e',
+      preset: 'ts-jest',
       testMatch: ['<rootDir>/tests/e2e/**/*.test.ts'],
       testEnvironment: 'node',
+      transform: {
+        '^.+\\.tsx?$': tsJestTransform
+      },
       setupFilesAfterEnv: ['<rootDir>/tests/e2e/setup.ts'],
       testTimeout: 60000
     }
